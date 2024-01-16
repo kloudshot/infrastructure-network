9ff5e07d71b12615bec16e67a2e4c275724c2a2b
ed-iune <ed@iune.co>
2024-01-16 16:15:45 +0400

    Implement protocols

---
+++ Sources/InfrastructureNetwork/Protocols/NetworkLogger.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public protocol NetworkLogger 
+{
+    func log(request: URLRequest)
+    func log(response: URLResponse, data: Data?)
+    func log(error: Error)
+}
---
+++ Sources/InfrastructureNetwork/Protocols/NetworkSession.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public protocol NetworkSession 
+{
+    func data(for request: URLRequest) async throws -> (Data, URLResponse)
+}
