f50e9eabd95f831ac06b3a0beeb82f3e1d888510
ed-iune <ed@iune.co>
2024-01-16 16:17:39 +0400

    Define Package

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "InfrastructureNetwork",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v12),
+        .watchOS(.v8),
+        .tvOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "InfrastructureNetwork",
+            targets: [
+                "InfrastructureNetwork"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/iune-co/infrastructure-dependency-container.git",
+            exact: Version("1.0.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "InfrastructureNetwork",
+            dependencies: [
+                .product(
+                    name: "InfrastructureDependencyContainer",
+                    package: "infrastructure-dependency-container"
+                )
+            ]
+        ),
+        .testTarget(
+            name: "InfrastructureNetworkTests",
+            dependencies: [
+                "InfrastructureNetwork"
+            ]
+        ),
+    ]
+)
